176506a4173a7e80f59fd9e25d0a25d56e7f2ba2
Olaitan Feranmi <olaitanferanmi41@gmail.com>
2023-12-25 02:05:32 -0800

    commit

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.VITE.BASE.PATH || '/deploy_react_app_github_pages_vercel',
+  
   plugins: [react()],
 })
