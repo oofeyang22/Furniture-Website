a34ddd757b05087076ee801003f1b249fb3239e6
Olaitan Feranmi <olaitanferanmi41@gmail.com>
2024-09-24 03:02:41 -0800

    Styling updated

--- vite-project/src/components/Testimonials.jsx
+++ vite-project/src/components/Testimonials.jsx
@@ -20,10 +20,10 @@ const Testimonials = () => {
         <div className='flex flex-col md:flex-row relative justify-center items-center gap-6 md:gap-[8rem]'>
             <div className='bg-white shadow-xl shadow-gray-500 rounded-xl w-[20rem] h-[22rem] flex flex-col gap-5 justify-center items-center'>
                 <div className='absolute top-12 md:top-[2.3rem]'>
-                    <h2 className='px-[8px] text-2xl md:text-xl'><span className=' text-4xl md:text-2xl font-bold'>"</span>They delivered exactly when they 
+                    <h2 className='px-[8px] text-2xl md:text-xl w-[20rem] md:w-[15rem]'><span className='md:text-2xl font-bold'>"</span>They delivered exactly when they 
                     promised to deliver and that's rare.</h2>
                 </div>
-                <div className='bg-green-900 absolute top-[17rem] md:top-[17rem] p-8 text-white rounded-xl w-[20rem] md:w-[17rem] h-[80px] text-center'>
+                <div className='bg-green-900 absolute top-[17rem] md:top-[17rem] p-8 text-white rounded-xl w-[20rem] md:w-[20rem] h-[80px] text-center'>
                     <p className='font-bold'>Anna Williams</p>
                     <p>Model</p>
                 </div>
@@ -34,10 +34,10 @@ const Testimonials = () => {
 
             <div className='bg-white shadow-xl shadow-gray-500 rounded-xl w-[20rem] h-[22rem] flex flex-col gap-5 justify-center items-center'>
                 <div className='absolute top-[26rem] md:top-[2.3rem]'>
-                    <h2 className='px-[8px] text-2xl md:text-xl md:w-[15rem]'><span className=' text-4xl md:text-2xl font-bold'>"</span>I bought their furniture and 
+                    <h2 className='px-[8px] text-2xl md:text-xl w-[20rem] md:w-[15rem]'><span className='md:text-2xl font-bold'>"</span>I bought their furniture and 
                     I have been using it for a long time</h2>
                 </div>
-                <div className='bg-[#D70404] absolute top-[41rem] md:top-[17rem] p-8 text-white rounded-xl w-[20rem] md:w-[17rem] h-[80px] text-center'>
+                <div className='bg-[#D70404] absolute top-[41rem] md:top-[17rem] p-8 text-white rounded-xl w-[20rem] md:w-[20rem] h-[80px] text-center'>
                     <p className='font-bold'>Jack Kruger</p>
                     <p>Dancer</p>
                 </div>
@@ -48,10 +48,10 @@ const Testimonials = () => {
 
             <div className='bg-white shadow-xl shadow-gray-500 rounded-xl w-[20rem] h-[22rem] flex flex-col gap-5 justify-center items-center'>
                 <div className='absolute top-[49rem] md:top-[2.3rem]'>
-                    <h2 className='px-[8px] text-2xl md:text-xl'><span className=' text-4xl md:text-2xl font-bold'>"</span>I benefitted from a promo 
+                    <h2 className='px-[8px] text-2xl w-[20rem] md:w-[15rem] md:text-xl'><span className='md:text-2xl font-bold'>"</span>I benefitted from a promo 
                     held last easter and I got a big discount</h2>
                 </div>
-                <div className='bg-blue-900 absolute top-[65rem] md:top-[17rem] p-8 text-white rounded-xl w-[20rem] md:w-[17rem] h-[80px] text-center'>
+                <div className='bg-blue-900 absolute top-[65rem] md:top-[17rem] p-8 text-white rounded-xl w-[20rem] md:w-[20rem] h-[80px] text-center'>
                     <p className='font-bold'>Ian Xavi</p>
                     <p>Comedian</p>
                 </div>
