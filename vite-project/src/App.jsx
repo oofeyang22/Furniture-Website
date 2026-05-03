2fa5e71474e5f91b0633cd47a41026824a20bdc7
oofeyang22 <olaitanferanmi41@gmail.com>
2026-05-03 19:24:29 +0100

    revision 1

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,42 +1,4 @@
-/*import React from 'react'
-import About from './components/About'
-import Navbar from './components/Navbar'
-import Blog from './components/Blog'
-import Hero from './components/Hero'
-import Goals from './components/Goals'
-import Products from './components/Products'
-import Testimonials from './components/Testimonials'
-import Footer from './components/Footer'
-import Cookie from './components/Cookie'
-import PrivacyPolicy from './components/Privacy'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-//import './App.css'
 
-function App() {
-  return (
-    <div>
-      
-          <Navbar/>
-          
-      
-                <Hero/>
-                <About/>
-                <Blog/>
-                <Products/>
-                <Goals/>
-                <Testimonials/>
-                <Cookie/>
-            
-          
-          <Footer/>
-          
-      
-    </div>
-  )
-}
-
-
-export default App*/
 
 import React from 'react'
 import { Routes, Route } from 'react-router-dom' // Remove Router import
